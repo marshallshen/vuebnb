841ae8cfdf3d6e7efc81cab980f29fd99af9dccb
Marshall Shen <shen.marshall@gmail.com>
2016-08-27 17:40:07 -0500

    v3.1 - create SCSS & HAML scaffold

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+	def show
+		@places = Place.all
+		render "show"
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root to: "home#show"
 	namespace :api do
 		get "hosts/:id", to: "hosts#show"
 		get "places/search", to: "places#search"
